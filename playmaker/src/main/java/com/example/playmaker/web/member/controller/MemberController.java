86ee75e13983a8c07126436bc744a79f262682d1
wonhecho <chowond02@naver.com>
2023-09-03 22:26:40 +0900

    swagger 추가

--- playmaker/src/main/java/com/example/playmaker/web/member/controller/MemberController.java
+++ playmaker/src/main/java/com/example/playmaker/web/member/controller/MemberController.java
@@ -4,6 +4,10 @@ import com.example.playmaker.security.PrincipalUserDetails;
 import com.example.playmaker.service.member.MemberService;
 import com.example.playmaker.service.teamoffer.TeamOfferService;
 import com.example.playmaker.web.member.dto.*;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,14 +29,25 @@ public class MemberController {
     private final MemberService memberService;
     private final TeamOfferService teamOfferService;
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "success")
+            , @ApiResponse(responseCode = "400", description = "Bad Request")
+            , @ApiResponse(responseCode = "404", description = "Not Found")
+            , @ApiResponse(responseCode = "500", description = "Internal Error")
+    })
+    @ApiOperation(
+            value = "로그인"
+            , notes = "로그인 방식 "
+    )
+
     @PostMapping("/join")
-    public ResponseEntity<String> join(@Valid @RequestBody MemberForm memberForm) {
+    public ResponseEntity<String> join(@Valid @RequestBody @ApiParam(value = "회원가입정보") MemberForm memberForm) {
         memberService.join(memberForm);
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginInfo> login(@Valid @RequestBody LoginForm loginForm) {
+    public ResponseEntity<LoginInfo> login(@Valid @RequestBody @ApiParam(value = "로그인 데이터") LoginForm loginForm) {
         return new ResponseEntity<>(memberService.login(loginForm), HttpStatus.OK);   //jwt 내부 id, username, role 들어감
     }
 
@@ -40,28 +55,40 @@ public class MemberController {
     //UserPage 쪽은 로그인자의 id와 userPage 주인의 id를 구분해야함
     //로그인자의 id는 jwt 안에 포함되있음.
     //userPage 주인의 id는 pathVariable 임
-
+    @ApiOperation(
+            value = "개인정보 페이지 정보"
+            , notes = "개인정보 데이터 조회"
+    )
     @GetMapping("/userPage/{memberId}")
-    public ResponseEntity<UserPageInfo> showUserPage(@PathVariable Long memberId) {
+    public ResponseEntity<UserPageInfo> showUserPage(@PathVariable @ApiParam(value = "조회하고자 하는 회원의 기본값") Long memberId) {
         return new ResponseEntity<>(memberService.showUserPage(memberId), HttpStatus.OK);
     }
-
+    @ApiOperation(
+            value = "개인정보 페이지 수정"
+            , notes = "개인정보 수정"
+    )
     @GetMapping("/userPage/{memberId}/edit")
-    public ResponseEntity<UserPageInfo> showEditUserPage(@PathVariable Long memberId) {
+    public ResponseEntity<UserPageInfo> showEditUserPage(@PathVariable @ApiParam(value = "수정할 개인 데이터") Long memberId) {
         return new ResponseEntity<>(memberService.showUserPage(memberId), HttpStatus.OK);
     }
-
+    @ApiOperation(
+            value = "개인정보 페이지 수정"
+            , notes = "개인정보 수정"
+    )
     @PostMapping("/userPage/{memberId}/edit")
-    public ResponseEntity<?> updateEditUserPage(@RequestPart(value = "userPageForm") UserPageForm userPageForm,
+    public ResponseEntity<?> updateEditUserPage(@RequestPart @ApiParam(value = "userPageForm") UserPageForm userPageForm,
                                                 @RequestPart(value = "image") MultipartFile file,
                                                 @PathVariable Long memberId) throws IOException {
         memberService.updateUserPage(memberId, userPageForm, file);
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
-
+    @ApiOperation(
+            value = "팀 입단 요청 확인 페이지"
+            , notes = "팀 입단 요청을 받은 것을 확인"
+    )
     @PostMapping("/userPage/{memberId}/offer")
-    public ResponseEntity<?> showOfferPage(@AuthenticationPrincipal PrincipalUserDetails principalUserDetails, @PathVariable Long memberId,
-                                           @RequestBody OfferForm offerForm) {
+    public ResponseEntity<?> showOfferPage(@AuthenticationPrincipal PrincipalUserDetails principalUserDetails, @PathVariable @ApiParam(value = "개인 기본값") Long memberId,
+                                           @RequestBody @ApiParam(value = "팀제안") OfferForm offerForm) {
 
         Long loginMemberId = principalUserDetails.getMember().getId();
         teamOfferService.offer(loginMemberId, memberId, offerForm);
--- playmaker/src/main/java/com/example/playmaker/web/team/controller/TeamController.java
+++ playmaker/src/main/java/com/example/playmaker/web/team/controller/TeamController.java
@@ -4,14 +4,17 @@ package com.example.playmaker.web.team.controller;
 import com.example.playmaker.service.team.TeamService;
 import com.example.playmaker.web.team.dto.TeamForm;
 import com.example.playmaker.web.team.dto.TeamInfo;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,23 +40,32 @@ public class TeamController {
             value = "팀 생성"
             , notes = "팀 데이터를 통해서 팀을 생성한다"
     )
-    @PostMapping()
-    public ResponseEntity<?>insertTeam(@RequestPart(value = "teamInfo") TeamForm teamDto,
-                                         @RequestPart(value = "image") MultipartFile file) throws IOException {
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE
+                ,MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?>insertTeam(@RequestPart @Parameter(description = "팀정보") TeamForm teamDto,
+                                       @RequestPart @Parameter(description = "팀이미지") MultipartFile file) throws IOException {
         log.info("insert : teamDto",teamDto);
         teamService.insertTeam(teamDto, file);
         return ResponseEntity.ok("success");
     }
+    @ApiOperation(
+            value = "팀 조회"
+            , notes = "팀 데이터를 조회한다"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> selectTeam (@ApiParam(value = "팀Id",required = true) @PathVariable String id){
         log.info("select : team");
         List<TeamInfo> info = teamService.selectTeam(id);
         return ResponseEntity.ok(info);
     }
+    @ApiOperation(
+            value = "팀 수정"
+            , notes = "팀 데이터를 수정한다"
+    )
     @PutMapping("/{id}")
-    public ResponseEntity<?> editTeam(@PathVariable Long id,
-                                      @RequestPart(value = "teamInfo") TeamForm teamDto,
-                                      @RequestPart(value = "image") MultipartFile file) throws IOException{
+    public ResponseEntity<?> editTeam(@ApiParam(value = "팀Id", required = true) @PathVariable Long id,
+                                      @ApiParam(value = "팀정보", required = true) @RequestPart(value = "teamInfo") TeamForm teamDto,
+                                      @ApiParam(value = "팀이미지") @RequestPart(value = "image") MultipartFile file) throws IOException{
         log.info("update : teamDto",teamDto);
         teamService.editTeam(id, teamDto, file);
         return ResponseEntity.ok("success");
