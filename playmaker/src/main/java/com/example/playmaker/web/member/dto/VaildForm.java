11e262cfcff078da31adf1f4e99d8e9459741557
wonhecho <chowond02@naver.com>
2023-12-03 00:28:00 +0900

    오타수정

--- playmaker/src/main/java/com/example/playmaker/web/member/dto/VaildForm.java
+++ playmaker/src/main/java/com/example/playmaker/web/member/dto/VaildForm.java
@@ -9,7 +9,7 @@ public class VaildForm {
     @NotBlank(message = "아이디는 필수값입니다")
     private String userId;
     @NotBlank(message =  "닉네임은 필수값입니다.")
-    private String nickName;
+    private String nickname;
     @NotBlank(message = "전화번호는 필수값입니다.")
     private String contact;
     @NotBlank(message = "이메일은 필수값입니다.")
